962b4a5d46148e3ae7de97f92a6564bf2d78f24c
Miloszel <m.branewicz@wp.pl>
2024-10-07 07:13:03 +0200

    [fix]: usunięcie printowania zmiennej

--- resources/views/front/investments/index.blade.php
+++ resources/views/front/investments/index.blade.php
@@ -42,7 +42,6 @@
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-gap-4">
                     @foreach ($investments as $investment)
                         <div class="col">
-                            @dump($investment->id)
                             <?php
                             $link = '';
                             switch ($investment->id) {
