16f59b6917569b7b8b157e17bd41f039412520be
Jacek <kontakt@jk-design.pl>
2025-01-16 10:25:37 +0100

    Wyszukiwarka

--- resources/views/layouts/partials/homesearch.blade.php
+++ resources/views/layouts/partials/homesearch.blade.php
@@ -2,44 +2,44 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-lg-10 col-xl-8 offset-lg-1 offset-xl-2">
-                <form action=""
-                    class="bg-secondary text-white rounded d-flex row-gap-0 flex-wrap flex-sm-nowrap search-form"
-                    autocomplete="off">
+                <form action="{{ route('developro.search.index') }}"
+                      method="get"
+                      class="bg-secondary text-white rounded d-flex row-gap-0 flex-wrap flex-sm-nowrap search-form"
+                      autocomplete="off">
                     <div
                         class="row row-cols-1 row-cols-md-2 row-cols-lg-4 row-gap-3 align-items-end px-30 py-3 w-md-100 pb-md-40 pb-20">
                         <p class="col-12 w-100 text-uppercase mb-0">Wyszukiwarka</p>
                         <div class="col">
                             <select name="city" id="city" class="form-select">
-                                <option value="0" selected>Miasto</option>
-                                <option value="Warszawa">Warszawa</option>
-                                <option value="Krakow">Krakow</option>
-                                <option value="Wroclaw">Wroclaw</option>
-                                <option value="Poznan">Poznan</option>
+                                <option value="" selected>Miasto</option>
+                                @foreach($cities as $c)
+                                    <option value="{{ $c->id }}" {{ request('city') == $c->id ? 'selected' : '' }}>{{ $c->name }}</option>
+                                @endforeach
                             </select>
                         </div>
                         <div class="col">
                             <select name="rooms" id="rooms" class="form-select">
-                                <option value="0" selected>Pokoje</option>
+                                <option value="">Pokoje</option>
                                 <option value="1">1</option>
                                 <option value="2">2</option>
                                 <option value="3">3</option>
                                 <option value="4">4</option>
+                                <option value="5">4</option>
                             </select>
                         </div>
                         <div class="col">
                             <select name="area" id="area" class="form-select">
-                                <option value="0" selected>Powierzchnia</option>
-                                <option value="1">30-50 m<sup>2</sup></option>
-                                <option value="2">51-70 m<sup>2</sup></option>
-                                <option value="3">71-90 m<sup>2</sup></option>
-                                <option value="4">91-110 m<sup>2</sup></option>
-                                <option value="5">> 110 m<sup>2</sup></option>
+                                <option value="">Powierzchnia</option>
+                                <option value="30-50" {{ request('area') == '30-50' ? 'selected' : '' }}>30-50 m²</option>
+                                <option value="51-70" {{ request('area') == '51-70' ? 'selected' : '' }}>51-70 m²</option>
+                                <option value="71-90" {{ request('area') == '71-90' ? 'selected' : '' }}>71-90 m²</option>
+                                <option value="91-110" {{ request('area') == '91-110' ? 'selected' : '' }}>91-110 m²</option>
+                                <option value="111-300" {{ request('area') == '111-300' ? 'selected' : '' }}>> 110 m²</option>
                             </select>
                         </div>
                         <div class="col">
-
                             <select name="advanced" id="advanced" class="form-select">
-                                <option value="0" selected>Zaawansowanie</option>
+                                <option value="">Zaawansowanie</option>
                                 <option value="1">Przedsprzedaż</option>
                                 <option value="2">Realizacja 25%</option>
                                 <option value="3">Realizacja 50%</option>
@@ -47,7 +47,6 @@
                                 <option value="5">Realizacja 100%</option>
                                 <option value="6">Gotowe do odbioru</option>
                             </select>
-
                         </div>
                     </div>
                     <div class="flex-fill">
