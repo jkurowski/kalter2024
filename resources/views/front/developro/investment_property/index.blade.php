5d53d4a89c93f9d5bba3e77d4e02f9a363de80b0
Jacek <kontakt@jk-design.pl>
2025-03-11 15:18:51 +0100

    Mieszkania z ceną promocyjną opublikowane na głównej stronie: potrzeba usunąć „przecinek” z wskazanych cen

--- resources/views/front/developro/investment_property/index.blade.php
+++ resources/views/front/developro/investment_property/index.blade.php
@@ -171,8 +171,8 @@
                                         <span class="fs-24">@money($property->price)</span>
                                     @else
                                         @if($property->promotion_price)
-                                            <span class="fs-24">@money($property->price - $property->promotion_price)</span>
-                                            <span class="fs-24">Rabat: @money($property->promotion_price)</span>
+                                            <span class="fs-24">@money($property->promotion_price)</span>
+                                            <span class="fs-24">Rabat: @money($property->price - $property->promotion_price)</span>
                                         @endif
                                         @if($property->price)
                                             <span class="text-body-emphasis opacity-50 fs-6 align-middle text-decoration-line-through">@money($property->price)</span>
--- resources/views/front/homepage/index.blade.php
+++ resources/views/front/homepage/index.blade.php
@@ -250,7 +250,7 @@
                                 @else
                                     @if($p->promotion_price)
                                         <span class="me-2">@money($p->promotion_price)</span>
-                                        <span class="fs-24">Rabat: @money($property->promotion_price)</span>
+                                        <span class="fs-24">Rabat: @money($p->price - $p->promotion_price)</span>
                                     @endif
                                     @if($p->price)
                                         <span class="text-body-emphasis opacity-50 fs-6 align-middle text-decoration-line-through">@money($p->price)</span>
