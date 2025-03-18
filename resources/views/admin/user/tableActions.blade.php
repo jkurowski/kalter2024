fd1926fb9a126f7a8fab7e81090751831a884f42
Jacek <kontakt@jk-design.pl>
2025-03-18 13:28:18 +0100

    Dostepy uzytkownikow

--- resources/views/admin/user/tableActions.blade.php
+++ resources/views/admin/user/tableActions.blade.php
@@ -9,14 +9,14 @@
         @endif
     @endcan
 
-    @can('user-self')
+    {{ Auth::user()->hasRole('Administrator') }}
+
+    @if(!Auth::user()->hasRole('Administrator'))
         @if($row->id == Auth::id())
             <a href="{{route('admin.user.edit', $row->id)}}" class="btn action-button me-1" data-bs-toggle="tooltip" data-placement="top" data-bs-title="Edytuj"><i class="fe-edit"></i></a>
         @endif
     @else
-        @can('user-edit')
         <a href="{{route('admin.user.edit', $row->id)}}" class="btn action-button me-1" data-bs-toggle="tooltip" data-placement="top" data-bs-title="Edytuj"><i class="fe-edit"></i></a>
-        @endcan
     @endif
 
     @can('user-delete')
