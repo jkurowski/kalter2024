30bd0ef2556988dc320dd18702e469c801c8dd73
Jacek <kontakt@jk-design.pl>
2025-03-18 12:56:25 +0100

    Dostepy uzytkownikow

--- resources/views/admin/user/form.blade.php
+++ resources/views/admin/user/form.blade.php
@@ -46,6 +46,7 @@
                                         'select' => $cities
                                     ])
                                 </div>
+                                @if(Auth::user()->role('Administrator'))
                                 <div class="row w-100 form-group">
                                     @isset($selected)
                                         @include('form-elements.html-select-multiple', ['label' => 'Role CRM', 'name' => 'roles', 'select' => $roles, 'selected' => $selected, 'required' => 1])
@@ -53,6 +54,7 @@
                                         @include('form-elements.html-select-multiple', ['label' => 'Role CRM', 'name' => 'roles', 'select' => $roles, 'required' => 1])
                                     @endif
                                 </div>
+                                @endif
                                 <div class="row w-100 form-group">
                                     @include('form-elements.html-textarea', [
                                           'label' => 'Podpis e-mail',
