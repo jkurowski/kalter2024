b4538265f0d8aa331a2236c4893d774bd69db86c
Jacek Kurowski <jacek@4dl.pl>
2025-07-10 16:47:23 +0200

    [Jacek][Fix] Naprawić zapis pow. bo zmienia się na typ 2

--- resources/views/admin/developro/investment_shared/related.blade.php
+++ resources/views/admin/developro/investment_shared/related.blade.php
@@ -1,10 +1,11 @@
 <tr>
     <td class="pe-0 text-center">
         <input type="checkbox" class="checkbox" name="property[]" id="{{ $property->id }}" value="{{ $property->id }}" style="display: none;">
-        <input type="hidden" name="type" value="{{ $property->type }}">
-        <span id="1"><i class="las la-trash-alt"></i></span>
+        <input type="hidden" name="related_type" value="{{ $property->type }}">
+        <span data-property="{{ $property->id }}" class="remove-related"><i class="las la-trash-alt"></i></span>
     </td>
     <td><a href="" target="_blank"><b>{{ $property->name }}</b></a></td>
+    <td class="text-center"><b>{{ $property->getLocation() }}</b></td>
     <td class="text-center">Pow.: <b>{{ $property->area }}</b></td>
     <td class="text-center">
         @if($property->price)
