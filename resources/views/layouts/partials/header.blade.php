ca37bf1bc148254f495b01aba56dd6ddf7910ac0
Jacek Kurowski <jacek@4dl.pl>
2026-03-23 17:41:12 +0100

    [Jacek][New] Ukrycie telefonu z boku strony na mobile

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -104,6 +104,11 @@
 
 <div id="call-bar" class="d-block d-sm-none call-bar hidden">
     <a href="tel:+48539975771" class="call-btn">
-        📞 Zadzwoń
+        <svg width="20" height="20" viewBox="0 0 24 24" fill="#FFA500">
+            <path d="M6.6 10.8c1.5 3 3.6 5.1 6.6 6.6l2.2-2.2c.3-.3.7-.4 1.1-.3
+        1.2.4 2.5.6 3.8.6.6 0 1 .4 1 1V21c0 .6-.4 1-1 1C10.3 22
+        2 13.7 2 3c0-.6.4-1 1-1h3.5c.6 0 1 .4 1 1
+        0 1.3.2 2.6.6 3.8.1.4 0 .8-.3 1.1l-2.2 2.2z"/>
+        </svg> Zadzwoń
     </a>
 </div>
