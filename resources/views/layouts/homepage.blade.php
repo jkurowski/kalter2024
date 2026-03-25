bdf8f35b8ffd567401abeae23584f1d7bd691e7a
Jacek Kurowski <jacek@4dl.pl>
2026-03-25 17:39:44 +0100

    [Jacek][New] Ukrywanie wyszukiwarki

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -5,27 +5,34 @@
 
     <title>{{ settings()->get("page_title") }}</title>
     <meta charset="utf-8">
-    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="{{ settings()->get("page_description") }}">
     <meta name="robots" content="{{ settings()->get("page_robots") }}">
     <meta name="author" content="{{ settings()->get("page_author") }}">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="/uploads/{{ settings()->get("page_favicon") }}">
 
+    <!-- FONTS (non-blocking) -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@700;900&family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}?v=2403v1" />
-    <link rel="stylesheet" href="{{ asset('css/slick.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/aos.css') }}" media="(min-width: 550px)" />
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@700;900&family=Open+Sans:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+          media="print"
+          onload="this.media='all'">
+
+    <!-- CSS (non-blocking) -->
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}?v=2403v1" media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="{{ asset('css/slick.css') }}" media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="{{ asset('css/aos.css') }}" media="(min-width: 550px)">
+
+    <!-- fallback dla no-js -->
+    <noscript>
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    </noscript>
 
     <!-- Preloads -->
-    <link rel="preload" href="{{ asset('img/logo.svg') }}" as="image" />
-    <!-- /Preloads -->
+    <link rel="preload" href="{{ asset('img/logo.svg') }}" as="image">
 
     @stack('style')
 </head>
