8a307913c91d75cd7c1916c1211b50ecf34a2956
Jacek Kurowski <jacek@4dl.pl>
2026-04-08 16:26:18 +0200

    [Jacek][New] Ukrywanie wyszukiwarki

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -102,9 +102,9 @@
 </footer>
 
 <script src="{{ asset('js/jquery.min.js') }}"></script>
-<script src="{{ asset('js/slick.min.js') }}" defer></script>
+<script src="{{ asset('js/slick.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('js/aos.js') }}" defer></script>
-<script src="{{ asset('js/glightbox.min.js') }}" defer></script>
-<script src="{{ asset('js/main.js') }}" defer></script>
+<script src="{{ asset('js/aos.js') }}"></script>
+<script src="{{ asset('js/glightbox.min.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
 <link rel="stylesheet" href="{{ asset('css/glightbox.min.css') }}" media="print" onload="this.media='all'">
