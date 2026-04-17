9d4bb79d036036d321ac373e95d389f877b7606b
Jacek Kurowski <jacek@4dl.pl>
2026-04-17 17:08:27 +0200

    [Jacek][New] Karta mieszkania w planowanej inwestycji

--- resources/views/front/developro/investment_building/index.blade.php
+++ resources/views/front/developro/investment_building/index.blade.php
@@ -57,12 +57,23 @@
                             </ol>
                         </nav>
                     </div>
-                    <div class="col-12 col-md-8 offset-md-2 col-xl-6 offset-xl-3 text-white text-center">
-                        @isset($investment->name)
+
+                    @isset($investment->entry_content)
+                        <div
+                            class="col-12 col-md-8 text-white text-center">
                             <h1 class="h2 mb-3 text-uppercase" data-aos="fade-up">{{ $investment->name }}</h1>
-                        @endisset
-                        <p class="text-pretty" data-aos="fade-up" data-aos-delay="200">{{ $building->name }}</p>
-                    </div>
+                            <p class="text-pretty" data-aos="fade-up" data-aos-delay="200">{{ $investment->entry_content }}</p>
+                        </div>
+                    @else
+                        <div class="col-12 col-md-8 offset-md-2 col-xl-6 offset-xl-3 text-white text-center">
+                            @isset($investment->name)
+                                <h1 class="h2 mb-3 text-uppercase" data-aos="fade-up">{{ $investment->name }}</h1>
+                            @endisset
+                            @isset($page->title_text)
+                                <p class="text-pretty" data-aos="fade-up" data-aos-delay="200">{{ $building->name }}</p>
+                            @endisset
+                        </div>
+                    @endisset
                 </div>
             </div>
         </section>
