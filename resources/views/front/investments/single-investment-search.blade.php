014338803781b672d68ec9adf8745c652a6afe72
Jacek Kurowski <jacek@4dl.pl>
2026-05-08 14:47:38 +0200

    [Jacek][Fix] poprawka w CMSie (do zrobienia)

--- resources/views/front/investments/single-investment-search.blade.php
+++ resources/views/front/investments/single-investment-search.blade.php
@@ -33,23 +33,12 @@
                                         </div>
                                     @endif
 
-                                    @if(!isset($is_building))
-                                        <div class="@if($status != 3) col @else col-12 col-sm-6 col-lg-3 @endif">
-                                            <select name="floor" id="floor" class="form-select">
-                                                <option value="">Piętro</option>
-                                                {!! floorToSelect($investment->floors) !!}
-                                            </select>
-                                        </div>
-                                    @endif
-
-                                    @if(isset($is_building))
-                                        <div class="@if($status != 3) col @else col-12 col-sm-6 col-lg-3 @endif">
-                                            <select name="floor" id="floor" class="form-select">
-                                                <option value="">Piętro</option>
-                                                {!! floorToSelect($investment->floors) !!}
-                                            </select>
-                                        </div>
-                                    @endif
+                                    <div class="@if($status != 3) col @else col-12 col-sm-6 col-lg-3 @endif">
+                                        <select name="floor" id="floor" class="form-select">
+                                            <option value="">Piętro</option>
+                                            {!! floorToSelect($investment->floors) !!}
+                                        </select>
+                                    </div>
 
                                     @if($status != 3)
                                     <div class="col">
